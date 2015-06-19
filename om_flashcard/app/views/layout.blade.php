cc78bee932b883ad69f2931cc791f839523ceb9a
Kazuhiro ike <ganbare.carp@gmail.com>
2015-06-19 20:37:12 +0900

    [user create] make view file

--- om_flashcard/app/views/layout.blade.php
+++ om_flashcard/app/views/layout.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lnag="en">
 
 <head>
+    <meta charset="utf-8">
     <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- Bootstrap -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    {{ HTML::style('css/bootstrap.css'); }}
 </head>
 <body>
     @yield('content')
+
+    {{ HTML::script('js/jquery-2.1.4.min.js'); }}
+    {{ HTML::script('js/bootstrap.min.js'); }}
 </body>
 </html>
--- om_flashcard/app/views/user/edit.blade.php
+++ om_flashcard/app/views/user/edit.blade.php
@@ -1,13 +1,24 @@
-@extends('...layout')
+@extends('  layout')
 
 @section('content')
 <div class="container" style="padding: 20px 0">
-    <h1>Users Edit</h1>
+    <h1>User Edit</h1>
     {{ Form::open(['url' => 'users/edit', 'class' => 'form-horizontal']) }}
         <div class="form-group">
-            <label class="col-sm-2 control-label">User Name</label>
-            <div class="col-sm-3">
+            <label class=" control-label col-sm-2">User Name</label>
+            <div class="col-sm-4">
                 {{ Form::text('username', null, ['class' => 'form-control'] ) }}
+            </div>
+        </div>
+        <div class="form-group">
+            <label class=" control-label col-sm-2">Email</label>
+            <div class="col-sm-4">
+                {{ Form::text('email', null, ['class' => 'form-control'] ) }}
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-4">
+                <input type="submit" value="submit" class="btn btn-primary">
             </div>
         </div>
     {{ Form::close() }}
